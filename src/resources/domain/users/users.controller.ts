468f87f93d46ac1226fb4efd127dc7e39043e408
Nathally Souza <nathallyccd@gmail.com>
2024-10-05 18:52:53 -0300

    [refactor]remove ununsed code

--- src/resources/domain/users/users.controller.ts
+++ src/resources/domain/users/users.controller.ts
@@ -1,19 +1,13 @@
-import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { UsersService } from './service/users.service';
-import { CreateUserDto } from '../dto/create-user.dto';
-import { User } from '../entities/user.entity';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Controller()
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.usersService.findOne(+id);
-  }
-
   @Post()
-  async create(@Body() createUserDto: CreateUserDto): Promise<User> {
+  async create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
 }
