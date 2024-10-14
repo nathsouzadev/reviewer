20c6a305cc8572777165797434ea77228ca09d7e
Nathally Souza <nathallyccd@gmail.com>
2024-10-14 20:36:54 -0300

    [feat]validate user to get review

--- src/resources/domain/revivew/service/revivew.service.spec.ts
+++ src/resources/domain/revivew/service/revivew.service.spec.ts
@@ -1,15 +1,23 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { RevivewService } from './revivew.service';
 import { GeminiService } from '../../../../resources/client/gemini/gemini.service';
+import { UsersService } from '../../users/service/users.service';
 
 describe('RevivewService', () => {
   let service: RevivewService;
   let mockGeminiService: GeminiService;
+  let mockUserService: UsersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         RevivewService,
+        {
+          provide: UsersService,
+          useValue: {
+            getByEmail: jest.fn(),
+          },
+        },
         {
           provide: GeminiService,
           useValue: {
@@ -21,9 +29,11 @@ describe('RevivewService', () => {
 
     service = module.get<RevivewService>(RevivewService);
     mockGeminiService = module.get<GeminiService>(GeminiService);
+    mockUserService = module.get<UsersService>(UsersService);
   });
 
   it('should return review', async () => {
+    const mockEmail = 'ada@reprograma.com.br';
     jest.spyOn(mockGeminiService, 'review').mockImplementation(() =>
       Promise.resolve({
         review: '## Code Review Feedback for PR #15',
@@ -31,14 +41,33 @@ describe('RevivewService', () => {
     );
 
     const mockReviewDto = {
-      email: 'ada@reprograma.com.br',
+      email: mockEmail,
       url: 'https://github.com/ada/repo/pull/15',
     };
 
     const response = await service.review(mockReviewDto);
+    expect(mockUserService.getByEmail).toHaveBeenCalledWith(mockEmail);
     expect(mockGeminiService.review).toHaveBeenCalledWith(mockReviewDto.url);
     expect(response).toMatchObject({
       review: '## Code Review Feedback for PR #15',
     });
+  });
+
+  it('should throw error if user not found', async () => {
+    const mockEmail = 'ada@reprograma.com.br';
+
+    const mockReviewDto = {
+      email: mockEmail,
+      url: 'https://github.com/ada/repo/pull/15',
+    };
+
+    jest.spyOn(mockUserService, 'getByEmail').mockImplementation(() => {
+      throw new Error('User not found');
+    });
+
+    await expect(service.review(mockReviewDto)).rejects.toThrowError(
+      'User not found',
+    );
+    expect(mockGeminiService.review).not.toHaveBeenCalled();
   });
 });
