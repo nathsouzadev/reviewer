a5fa41ce304caf54b65f1ca6f586c52667c0a22e
Nathally Souza <nathallyccd@gmail.com>
2024-10-14 20:17:07 -0300

    [refactor]add return tyoe

--- src/resources/domain/users/repository/users.repository.ts
+++ src/resources/domain/users/repository/users.repository.ts
@@ -1,3 +1,4 @@
+import { DeleteResult, UpdateResult } from 'typeorm';
 import { User } from '../../../../config/db/entities/users.entity';
 import { UpdateUserDto } from '../dto/update-user.dto';
 
@@ -5,6 +6,6 @@ export abstract class UserRepository {
   abstract create(user: User): Promise<User>;
   abstract get(): Promise<User[]>;
   abstract getById(id: string): Promise<User>;
-  abstract update(id: string, user: UpdateUserDto);
-  abstract delete(id: string);
+  abstract update(id: string, user: UpdateUserDto): Promise<UpdateResult>;
+  abstract delete(id: string): Promise<DeleteResult>;
 }
